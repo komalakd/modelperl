964a154e0419aa0fdae6bffe3f0f3b76190a7b1a
Andrés Valle <andresvalle.isi@gmail.com>
2013-11-22 17:45:20 -0300

    Updated dump file.

--- database/dump.sql
+++ database/dump.sql
@@ -1,41 +1,58 @@
-/*
-SQLyog Ultimate v9.63 
-MySQL - 5.5.8-log : Database - modelperl
-*********************************************************************
-*/
+CREATE DATABASE  IF NOT EXISTS `modelperl` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
+USE `modelperl`;
+-- MySQL dump 10.13  Distrib 5.5.31, for debian-linux-gnu (i686)
+--
+-- Host: 127.0.0.1    Database: modelperl
+-- ------------------------------------------------------
+-- Server version	5.5.31-0ubuntu0.12.04.1
 
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`modelperl` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
 
-USE `modelperl`;
+--
+-- Table structure for table `musicos`
+--
 
 DROP TABLE IF EXISTS `musicos`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `musicos` (
-  `id_musico` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(25) NOT NULL,
-  `apellido` varchar(25) NOT NULL,
-  `telefono_fijo` varchar(25) DEFAULT NULL,
-  `telefono_celular` varchar(25) DEFAULT NULL,
+  `id_musico` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `apellido` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `telefono_fijo` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `telefono_celular` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
   `fecha_alta` date NOT NULL,
-  `id_complejo` int(11) unsigned NOT NULL,
-  PRIMARY KEY (`id_musico`),
-  KEY `id_complejo` (`id_complejo`),
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+  `id_complejo` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `musicoscol` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id_musico`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Table structure for table `precios_salas` */
+--
+-- Dumping data for table `musicos`
+--
 
-DROP TABLE IF EXISTS `precios_salas`;
+LOCK TABLES `musicos` WRITE;
+/*!40000 ALTER TABLE `musicos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `musicos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-11-22 17:44:06
